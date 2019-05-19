a962f41301e8e37b32c14f7a7a0d0f819dd8ddbb
AbduEndrisM <beazsasm@gmail.com>
2019-05-19 06:13:31 -0500

    Coupon module done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categories');
+const couponsController = require('../controllers/coupons');
 
 router.get('/categories', categoryController.getAll);
 router.post('/categories', categoryController.add);
+
+router.get('/coupons', couponsController.getCoupons);
+router.post('/coupons', couponsController.addCoupon);
+router.patch('/coupons', couponsController.updateCoupon);
+router.delete('/coupons/:id', couponsController.deleteCoupon);
+
 
 module.exports = router;
