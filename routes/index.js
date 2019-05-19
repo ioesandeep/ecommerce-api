ea66898c1a46aec81f73be846e3603b3c7c9bdda
Sandeep Giri <ioesandeep@gmail.com>
2019-05-19 06:58:00 -0500

    Category api setup

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const categoryController = require('../controllers/categories');
 
 router.get('/categories', categoryController.getAll);
 router.post('/categories', categoryController.add);
+router.patch('/categories/:id', categoryController.update);
+router.delete('/categories/:id', categoryController.delete);
 
 module.exports = router;
