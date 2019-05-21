491260e58b25f72db179367f2be5db6ec301a228
Sandeep Giri <ioesandeep@gmail.com>
2019-05-21 03:27:06 -0500

    middlewares setup

--- controllers/order/order.service.js
+++ controllers/order/order.service.js
@@ -1,13 +1,42 @@
-const mongo = require('../../db');
 const {ObjectID} = require('mongodb');
+const stripe = require('stripe')('sk_test_4quWezbnkmlhLybWd0CGmmom00976mBzp4');
+const mongo = require('../../db');
 
 class OrderService {
+
+    async chargeViaStripe(order) {
+        const user = {
+            email: order.user.email,
+            source: order.payment.id
+        };
+
+        const customer = await stripe
+            .customers
+            .create(user);
+        order.stripe = customer;
+
+        const res = await stripe
+            .charges
+            .create({
+                amount: parseInt(order.total * 100),
+                currency: "usd",
+                customer: customer.id,
+            });
+
+        order.stripe.charge = res;
+    }
+
     async addAnOrder(data) {
         const db = await mongo.db();
+
+        await this.chargeViaStripe(data);
+        data.status = 1;
+        data.orderDate = new Date();
         const res = await db.collection('orders').insertOne(data);
         if (!res || !res.ops) {
             throw new Error("Order could not be saved.");
         }
+
         return res.ops[0];
     }
 
---
+++ middlewares/admin.middleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const mongo = require('../db');
+const {ObjectID} = require('mongodb');
+
+class AdminMiddleware {
+
+    async verify(req, res, next) {
+        try {
+            const u = jwt.verify(req.headers.authorization.replace('Bearer ', ''), 'mwa2019');
+            if (!u || !u._id) {
+                throw new Error("Request not authorized");
+            }
+
+            const db = await mongo.db();
+            const user = await db.collection('admins').findOne({_id: ObjectID(u._id)});
+            if (!user || !user._id) {
+                throw new Error("User does not exist.");
+            }
+
+            return next();
+        } catch (e) {
+            res.status(403);
+            next(e);
+        }
+    }
+}
+
+module.exports = new AdminMiddleware();
